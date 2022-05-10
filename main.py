60e9eccf6dffddeddc9ec8ebb9c7960ee6e40ba7
bhaskar reddy <45232937+Bhaskarrd10@users.noreply.github.com>
2022-05-10 11:30:12 +0000

    adding while loop

--- main.py
+++ main.py
@@ -13,11 +13,19 @@ for _ in range(word_length):
     dispaly += "_"
 print(dispaly)
 
+end_of_game = False
 
-guess = input("Guess a letter ").lower()
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        dispaly[position] = letter
+while not end_of_game:
+    guess = input("Guess a letter ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(f"Current position:{position}\n Current letter: {letter}\n                 Gussed letter: {guess}")
+        if letter == guess:
+            dispaly[position] = letter
+        
+    print(dispaly)
+
+    if "_" not in dispaly:
+        end_of_game = True
+        print("You win")
     
-print(dispaly)
