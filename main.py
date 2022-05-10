d8bcaff49dc2848c00c7f6250cb687b3a32c06ec
bhaskar reddy <45232937+Bhaskarrd10@users.noreply.github.com>
2022-05-10 10:34:19 +0000

    hungman

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import random
 word_list = ["Bottle","Love", "marrege"]
 
 chosen_word = random.choice(word_list)
+print(f"Passt, the solution is {chosen_word}")
 
-
+dispaly = []
+for _ in range(len(chosen_word)):
+    dispaly += "_"
+print(dispaly)
 guess = input("Guess a letter ").lower
+
+
+
 
 for letter in chosen_word:
     if letter == guess:
